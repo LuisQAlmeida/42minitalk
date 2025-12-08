39e6548805fb441f8a89d334fd236c3d9318af34
Luis Quental Almeida <luis.quental.almeida@gmail.com>
2025-12-08 16:02:50 +0000

    Prepare sigaction structure for handling actions related to each signal (SIGUSR 1 & 2) in server.c

--- minitalk/src/server.c
+++ minitalk/src/server.c
@@ -2,8 +2,12 @@
 
 int	main(void)
 {
-	ft_printf("Server PID is %d\n", getpid());
+	struct sigaction	sigact;
 
+	sigemptyset(&sigact.sa_mask);
+	sigact.sa_flags = SA_SIGINFO;
+	sigact.sa_sigaction = signal_handler_function;
+	ft_printf("Server PID is %d\n", getpid());
 	while (1)
 		pause();
 	return (0);
