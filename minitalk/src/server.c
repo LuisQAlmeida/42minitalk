6bf985d594e911f9fb53449a39c5708d8e17ec43
Luis Quental Almeida <luis.quental.almeida@gmail.com>
2025-12-09 13:33:34 +0000

    Update server.c to include only mandatory part: replace 3 argument handler by simple 1 argument handler to get only sig_nbr; replace SA_SIGINFO info flag with 0

--- minitalk/src/server.c
+++ minitalk/src/server.c
@@ -1,27 +1,19 @@
 #include "minitalk.h"
 
-static void	signal_handler(int sig_nbr, siginfo_t *info, void *context)
+static void	signal_handler(int sig_nbr)
 {
 	static int				bit_idx;
 	static unsigned char	full_char;
 
 	bit_idx = 0;
 	full_char = 0;
-	(void)context;
 	if (sig_nbr == SIGUSR2)
 		full_char |= (1 << (7 - bit_idx));
 	bit_idx++;
 	if (bit_idx == 8)
 	{
 		if (full_char == 0)
-		{
 			write(1, "\n", 1);
-			if (info && info->si_pid != 0)
-			{
-				if (kill(info->si_pid, SIGUSR1) == -1)
-					write(2, "Error: ACK Failed\n", 18);
-			}
-		}
 		else
 			write(1, &full_char, 1);
 		bit_idx = 0;
@@ -34,8 +26,8 @@ int	main(void)
 	struct sigaction	sigact;
 
 	sigemptyset(&sigact.sa_mask);
-	sigact.sa_flags = SA_SIGINFO;
-	sigact.sa_sigaction = signal_handler;
+	sigact.sa_flags = 0;
+	sigact.sa_handler = signal_handler;
 	ft_printf("Server PID is %d\n", getpid());
 	if (sigaction(SIGUSR1, &sigact, NULL) == -1
 		|| sigaction(SIGUSR2, &sigact, NULL) == -1)
