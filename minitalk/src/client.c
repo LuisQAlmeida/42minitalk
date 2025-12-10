e87a4fe58eea31592bbe883eef6bd966ec2a6dfe
Luis Quental Almeida <luis.quental.almeida@gmail.com>
2025-12-10 09:35:54 +0000

    Delete everything in client.c's main, except for error handling for invalid input. Remove '\n' from error string - ft_error already handles this.

--- minitalk/src/client.c
+++ minitalk/src/client.c
@@ -2,15 +2,7 @@
 
 int	main(int argc, char **argv)
 {
-	int	i;
-
-	i = 0;
 	if (argc != 3)
-		ft_error("Please type: ./client <Server PID> <Message>\n", 1);
-	while (argv[2][i])
-	{
-		write(1, &argv[2][i], 1);
-		i++;
-	}
+		ft_error("Please type: ./client <Server PID> <Message>", 1);
 	return (0);
 }
