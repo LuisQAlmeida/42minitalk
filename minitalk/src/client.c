f9d4a345496258f13bb665055a8d2dd4d5ad9b5a
Luis Quental Almeida <luis.quental.almeida@gmail.com>
2025-12-10 09:41:17 +0000

    Convert PID string to integer using ft_atoi and typecast it to pid_t.

--- minitalk/src/client.c
+++ minitalk/src/client.c
@@ -2,7 +2,10 @@
 
 int	main(int argc, char **argv)
 {
+	pid_t	server_pid;
+
 	if (argc != 3)
 		ft_error("Please type: ./client <Server PID> <Message>", 1);
+	server_pid = (pid_t)ft_atoi(argv[1]);
 	return (0);
 }
