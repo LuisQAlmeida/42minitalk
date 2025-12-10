e908869e8cff6d086a091f6a5bec1f36aeba25bf
Luis Quental Almeida <luis.quental.almeida@gmail.com>
2025-12-10 11:27:39 +0000

    Add error handling for invalid server PID format (through valid_pid_format function) and value. Add placeholder comment for message sending function.

--- minitalk/src/client.c
+++ minitalk/src/client.c
@@ -1,11 +1,32 @@
 #include "minitalk.h"
 
+int	valid_pid_format_(const char *srv_pid)
+{
+	int	i;
+
+	i = 0;
+	if (!srv_pid || !srv_pid[i])
+		return (0);
+	while (srv_pid[i] != '\0')
+	{
+		if (!is_digit(srv_pid[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
 	pid_t	server_pid;
 
 	if (argc != 3)
 		ft_error("Please type: ./client <Server PID> <Message>", 1);
+	if (!valid_pid_format)
+		ft_error("Error: Invalid Server PID format.", 2);
 	server_pid = (pid_t)ft_atoi(argv[1]);
+	if (server_pid <= 0)
+		ft_error("Error: Invalid Server PID value.", 3);
+	/* ------> Function to send message to Server PID here. <------- */
 	return (0);
 }
