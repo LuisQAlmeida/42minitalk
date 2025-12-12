e7c6e6854b0b71421a5b180b758be48b802b177a
Luis Quental Almeida <luis.quental.almeida@gmail.com>
2025-12-12 10:11:05 +0000

    Implement message sending from client to server.

--- minitalk/src/client.c
+++ minitalk/src/client.c
@@ -18,9 +18,39 @@ int	valid_pid_format(const char *srv_pid)
 	return (1);
 }
 
-void	send_msg(pid_t srv_pid, const char *msg)
+static void	send_char(pid_t srv_pid, unsigned char c)
 {
-	
+	int	bit_idx;
+
+	bit_idx = 7;
+	while (bit_idx >= 0)
+	{
+		if ((c >> bit_idx) & 1)
+		{
+			if (kill(srv_pid, SIGUSR2) == -1)
+				ft_error("Error: Failed To Send Signal.", 4);
+		}
+		else
+		{
+			if (kill(srv_pid, SIGUSR1) == -1)
+				ft_error("Error: Failed To Send Signal.", 4);
+		}
+		usleep(100);
+		bit_idx--;
+	}
+}
+
+static void	send_msg(pid_t srv_pid, const char *msg)
+{
+	int	i;
+
+	i = 0;
+	while (msg[i] != '\0')
+	{
+		send_char(srv_pid, (unsigned char)msg[i]);
+		i++;
+	}
+	send_char(srv_pid, '\0');
 }
 
 int	main(int argc, char **argv)
@@ -34,6 +64,6 @@ int	main(int argc, char **argv)
 	server_pid = (pid_t)ft_atoi(argv[1]);
 	if (server_pid <= 0)
 		ft_error("Error: Invalid Server PID value.", 3);
-	/* ------> Function to send message to Server PID here. <------- */
+	send_msg(server_pid, argv[2]);
 	return (0);
 }
